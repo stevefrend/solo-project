80bfdd994b9a18e05bfa110b8bee9ccfb801b19b
Steve Frend <stevefrend1@gmail.com>
2020-04-16 14:31:07 -0700

    making style changes

--- src/components/Login.component.jsx
+++ src/components/Login.component.jsx
@@ -4,14 +4,16 @@ const Login = (props) => {
 
   return (
     <div className='displayContainer'>
-      <h1>Login</h1>
+      <h1>Welcome to the RawFeeding App</h1>
+      <h2>Login</h2>
       <div className="form-wrapper">
         <form className='login-form' onSubmit={props.validateLogin}>
           <input className="loginInput" type='text' placeholder='Username' />
           <input className="loginInput" placeholder='Password' type='password' />
-          <button className="loginInput" type='submit'>Enter</button>
+          <button className="loginButton" type='submit'>Enter</button>
         </form>
       </div>
+      <p>Or SIGN UP! <br/>We add lots of salt to your password<br/> so your data is IMPOSSIBLE TO HACK!</p>
       <button className='signupButton' onClick={props.renderSignupPage}>
         Sign up
       </button>
